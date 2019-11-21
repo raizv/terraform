3975b2793a6dca31d3899663f88609b9fa7b3391
Anton Raizvikh <raizvih@gmail.com>
2019-11-21 10:00:28 -0800

    rename development_services to project_services

--- development/main.tf
+++ development/main.tf
@@ -5,7 +5,7 @@ resource "google_project" "project" {
   billing_account = data.google_billing_account.account.id
 }
 
-module "development_services" {
+module "project_services" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
   version = "3.3.0"
 
@@ -13,11 +13,11 @@ module "development_services" {
   activate_apis = [
     "compute.googleapis.com",
     "container.googleapis.com",
+    "dns.googleapis.com",
     "monitoring.googleapis.com",
     "stackdriver.googleapis.com",
     "cloudbuild.googleapis.com",
     "cloudtrace.googleapis.com",
-    "dns.googleapis.com",
     "containeranalysis.googleapis.com"
   ]
   disable_services_on_destroy = false
@@ -31,11 +31,11 @@ resource "google_compute_network" "vpc" {
   auto_create_subnetworks = "false"
 }
 
-# module "development_us_west1_network" {
+# module "us_west1_network" {
 #   source = "./modules/network"
 
-#   project = google_project.development.project_id
-#   network = google_compute_network.development_vpc.self_link
+#   project = google_project.project.project_id
+#   network = google_compute_network.project_vpc.self_link
 #   region  = "us-west1"
 
 #   ip_cidr_range         = "10.1.0.0/16"
@@ -44,15 +44,15 @@ resource "google_compute_network" "vpc" {
 # }
 
 # Cluster configuration
-# module "development_us_west1_cluster" {
+# module "us_west1_cluster" {
 #   # source           = "github.com/raizv/terraform-gke?ref=v0.0.1"
 #   source = "./modules/cluster"
 
 #   name                   = "development-us-west1"
 #   location               = "us-west1"
-#   network                = google_compute_network.development_vpc.self_link
+#   network                = google_compute_network.vpc.self_link
 #   subnetwork             = module.development_us_west1_network.subnetwork.self_link
-#   project                = google_project.development.project_id
+#   project                = google_project.project.project_id
 #   organization           = var.organization_domain
 #   service_account        = module.development_service_account.email
 #   master_ipv4_cidr_block = "172.18.0.0/28"
@@ -65,7 +65,7 @@ resource "google_compute_network" "vpc" {
 #   name        = "dev-us-west1"
 #   dns_name    = "dev-us-west1.raizv.ca."
 #   description = "Zone for dev-us-west1 cluster"
-#   project     = google_project.development.project_id
+#   project     = google_project.project.project_id
 # }
 
 # TLS cert
@@ -73,18 +73,18 @@ resource "google_compute_network" "vpc" {
 #   provider = "google-beta"
 
 #   name    = "dev-us-west1"
-#   project = google_project.development.project_id
+#   project = google_project.project.project_id
 #   managed {
 #     domains = ["dev-us-west1.raizv.ca."]
 #   }
 # }
 
-# module "development_service_account" {
+# module "gke_service_account" {
 #   source = "./modules/service_account"
 
 #   name        = "gke-cluster"
 #   description = "Development GKE service account"
-#   project     = google_project.development.project_id
+#   project     = google_project.project.project_id
 # }
 
 # # Add dns.admin role to external-dns service account
@@ -93,20 +93,20 @@ resource "google_compute_network" "vpc" {
 
 #   name                  = "external-dns"
 #   description           = "Service Account for External DNS service running in GKE"
-#   project               = google_project.development.project_id
+#   project               = google_project.project.project_id
 #   service_account_roles = ["roles/dns.admin"]
 # }
 
 # # Allow service account in GKE to use Workload Identity
 # resource "google_project_iam_member" "external_dns_identity_user_role" {
-#   project = google_project.development.project_id
+#   project = google_project.project.project_id
 #   role    = "roles/iam.workloadIdentityUser"
-#   member  = "serviceAccount:${google_project.development.project_id}.svc.id.goog[kube-system/external-dns]"
+#   member  = "serviceAccount:${google_project.project.project_id}.svc.id.goog[kube-system/external-dns]"
 # }
 
 # # Allow Cloud Build to deploy to GKE
 # resource "google_project_iam_member" "cloudbuild_deploy" {
-#   project = google_project.development.project_id
+#   project = google_project.project.project_id
 #   role    = "roles/container.developer"
-#   member  = "serviceAccount:${google_project.development.number}@cloudbuild.gserviceaccount.com"
+#   member  = "serviceAccount:${google_project.project.number}@cloudbuild.gserviceaccount.com"
 # }
