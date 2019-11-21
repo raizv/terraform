1c7b8ec12fd01caef12dec6a13ccb502035edd49
Anton Raizvikh <raizvih@gmail.com>
2019-11-21 15:07:33 -0800

    disable router and nat

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -17,24 +17,24 @@ resource "google_compute_subnetwork" "subnetwork" {
   }
 }
 
-resource "google_compute_router" "router" {
-  name    = var.router_name
-  region  = var.region
-  network = var.network
-  project = var.project
-}
+# resource "google_compute_router" "router" {
+#   name    = var.router_name
+#   region  = var.region
+#   network = var.network
+#   project = var.project
+# }
 
-resource "google_compute_router_nat" "nat" {
-  name    = var.nat_name
-  router  = google_compute_router.router.name
-  region  = var.region
-  project = var.project
+# resource "google_compute_router_nat" "nat" {
+#   name    = var.nat_name
+#   router  = google_compute_router.router.name
+#   region  = var.region
+#   project = var.project
 
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+#   nat_ip_allocate_option             = "AUTO_ONLY"
+#   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
-  }
-}
+#   log_config {
+#     enable = true
+#     filter = "ERRORS_ONLY"
+#   }
+# }
