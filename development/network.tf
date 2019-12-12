0b796e6bd3652d0621668e1362eff2608a6ddee3
Anton Raizvikh <raizvih@gmail.com>
2019-12-12 14:28:14 -0800

    Delete GKE and network resources

--- development/network.tf
+++ development/network.tf
@@ -8,14 +8,14 @@ resource "google_compute_network" "vpc" {
 }
 
 # Create Subnets, Router and NAT
-module "network" {
-  source = "../modules/network"
+# module "network" {
+#   source = "../modules/network"
 
-  project = google_project.project.project_id
-  network = google_compute_network.vpc.self_link
-  region  = "northamerica-northeast1"
+#   project = google_project.project.project_id
+#   network = google_compute_network.vpc.self_link
+#   region  = "northamerica-northeast1"
 
-  ip_cidr_range         = "10.1.0.0/16"
-  pod_ip_cidr_range     = "10.101.0.0/16" # cluster_secondary_range_name in gke.tf
-  service_ip_cidr_range = "10.201.0.0/16" # services_secondary_range_name in gke.tf
-}
+#   ip_cidr_range         = "10.1.0.0/16"
+#   pod_ip_cidr_range     = "10.101.0.0/16" # cluster_secondary_range_name in gke.tf
+#   service_ip_cidr_range = "10.201.0.0/16" # services_secondary_range_name in gke.tf
+# }
