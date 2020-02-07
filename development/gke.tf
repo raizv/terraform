70e4902193616087c59498ceeef6e3c76ff37e95
Anton Raizvikh <raizvih@gmail.com>
2020-02-07 13:11:31 -0800

    Create gke cluster

--- development/gke.tf
+++ development/gke.tf
@@ -11,37 +11,37 @@ module "network_us_central1" {
   network = google_compute_network.vpc.self_link
 }
 
-# module "gke_cluster" {
-#   source = "../modules/gke_cluster"
+module "gke_cluster" {
+  source = "../modules/gke_cluster"
 
-#   name                       = "development"
-#   location                   = "us-central1-a"
-#   master_ipv4_cidr_block     = "172.18.0.0/28"
-#   release_channel            = "REGULAR"
-#   enable_pod_security_policy = false
-#   enable_network_policy      = false
-#   disable_network_policy     = true
+  name                       = "development"
+  location                   = "us-central1-a"
+  master_ipv4_cidr_block     = "172.18.0.0/28"
+  release_channel            = "REGULAR"
+  enable_pod_security_policy = false
+  enable_network_policy      = false
+  disable_network_policy     = true
 
-#   # TODO: add psp_enabled = false
+  # TODO: add psp_enabled = false
 
-#   project                       = google_project.project.project_id
-#   network                       = google_compute_network.vpc.self_link
-#   subnetwork                    = module.network_us_central1.subnetwork.self_link
-#   cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
-#   services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
-#   org_domain                    = var.org_domain
-# }
+  project                       = google_project.project.project_id
+  network                       = google_compute_network.vpc.self_link
+  subnetwork                    = module.network_us_central1.subnetwork.self_link
+  cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
+  services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
+  org_domain                    = var.org_domain
+}
 
-# module "gke_node_pool" {
-#   source = "../modules/gke_node_pool"
+module "gke_node_pool" {
+  source = "../modules/gke_node_pool"
 
-#   cluster        = module.gke_cluster.name
-#   location       = "us-central1-a"
-#   machine_type   = "e2-standard-2"
-#   min_node_count = 1
-#   max_node_count = 3
-#   preemptible    = true
+  cluster        = module.gke_cluster.name
+  location       = "us-central1-a"
+  machine_type   = "e2-standard-2"
+  min_node_count = 1
+  max_node_count = 3
+  preemptible    = true
 
-#   project         = google_project.project.project_id
-#   service_account = module.gke_service_account.email
-# }
+  project         = google_project.project.project_id
+  service_account = module.gke_service_account.email
+}
