c63e30989b2bc2ba679b9a4090b07d2a74cc53dd
Anton Raizvikh <raizvih@gmail.com>
2020-02-10 15:18:58 -0800

    Create test cluster

--- development/gke.tf
+++ development/gke.tf
@@ -11,37 +11,71 @@ module "network_us_central1" {
   network = google_compute_network.vpc.self_link
 }
 
-module "gke_cluster" {
-  source = "../modules/gke_cluster"
+# module "gke_cluster" {
+#   source = "../modules/gke_cluster"
 
-  name                       = "development"
-  location                   = "us-central1-a"
-  master_ipv4_cidr_block     = "172.18.0.0/28"
-  release_channel            = "REGULAR"
-  enable_pod_security_policy = false
-  enable_network_policy      = false
-  disable_network_policy     = true
+#   name                       = "development"
+#   location                   = "us-central1-a"
+#   master_ipv4_cidr_block     = "172.18.0.0/28"
+#   release_channel            = "REGULAR"
+#   enable_pod_security_policy = false
+#   enable_network_policy      = false
+#   disable_network_policy     = true
 
-  # TODO: add psp_enabled = false
+#   # TODO: add psp_enabled = false
 
-  project    = google_project.project.project_id
+#   project    = google_project.project.project_id
+#   network    = google_compute_network.vpc.self_link
+#   subnetwork = module.network_us_central1.subnetwork.self_link
+#   # cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
+#   # services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
+#   org_domain = var.org_domain
+# }
+
+# module "gke_node_pool" {
+#   source = "../modules/gke_node_pool"
+
+#   cluster        = module.gke_cluster.name
+#   location       = "us-central1-a"
+#   machine_type   = "e2-standard-2"
+#   min_node_count = 1
+#   max_node_count = 3
+#   preemptible    = true
+
+#   project         = google_project.project.project_id
+#   service_account = module.gke_service_account.email
+# }
+
+resource "google_container_cluster" "cluster" {
+  provider   = google-beta
+  name       = "cluster"
+  location   = "us-central1-a"
   network    = google_compute_network.vpc.self_link
   subnetwork = module.network_us_central1.subnetwork.self_link
-  # cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
-  # services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
-  org_domain = var.org_domain
-}
+  project    = google_project.project.project_id
 
-module "gke_node_pool" {
-  source = "../modules/gke_node_pool"
+  # remove_default_node_pool = true
+  initial_node_count = 1
 
-  cluster        = module.gke_cluster.name
-  location       = "us-central1-a"
-  machine_type   = "e2-standard-2"
-  min_node_count = 1
-  max_node_count = 3
-  preemptible    = true
+  master_auth {
+    username = ""
+    password = ""
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
 
-  project         = google_project.project.project_id
-  service_account = module.gke_service_account.email
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 }
