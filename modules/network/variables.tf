ae22c9b92b6b80126bacea0dd644cb3af066ffea
Anton Raizvikh <raizvih@gmail.com>
2020-02-10 14:35:42 -0800

    Comment pod_ip_cidr_range

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -3,5 +3,5 @@ variable "network" {}
 variable "region" {}
 
 variable "ip_cidr_range" {}
-variable "pod_ip_cidr_range" {}
-variable "service_ip_cidr_range" {}
+# variable "pod_ip_cidr_range" {}
+# variable "service_ip_cidr_range" {}
