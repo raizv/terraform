352a3d6a51bcd6b718d2bd942d2e5835c05b2117
Anton Raizvikh <raizvih@gmail.com>
2020-02-11 15:54:18 -0800

    Delete gke cluster

--- development/gke.tf
+++ development/gke.tf
@@ -11,83 +11,30 @@ module "network_us_central1" {
   network = google_compute_network.vpc.self_link
 }
 
-module "gke_cluster" {
-  source = "../modules/gke_cluster"
+# module "gke_cluster" {
+#   source = "../modules/gke_cluster"
 
-  name     = "development"
-  location = "us-central1-a"
-  # master_ipv4_cidr_block     = "172.18.0.0/28"
-  release_channel            = "REGULAR"
-  enable_pod_security_policy = false
-  enable_network_policy      = false
-  disable_network_policy     = true
+#   name     = "development"
+#   location = "us-central1-a"
+#   # master_ipv4_cidr_block     = "172.18.0.0/28"
+#   release_channel            = "REGULAR"
+#   enable_pod_security_policy = false
+#   enable_network_policy      = false
+#   disable_network_policy     = true
 
-  # TODO: add psp_enabled = false
-
-  project    = google_project.project.project_id
-  network    = google_compute_network.vpc.self_link
-  subnetwork = module.network_us_central1.subnetwork.self_link
-  # cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
-  # services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
-  org_domain = var.org_domain
-}
-
-module "gke_node_pool" {
-  source = "../modules/gke_node_pool"
-
-  cluster        = module.gke_cluster.name
-  location       = "us-central1-a"
-  machine_type   = "e2-standard-2"
-  min_node_count = 1
-  max_node_count = 3
-  preemptible    = true
-
-  project         = google_project.project.project_id
-  service_account = module.gke_service_account.email
-}
-
-
-# resource "google_container_cluster" "primary" {
-#   provider   = google-beta
-#   name       = "primary"
-#   location   = "us-central1-a"
-#   project    = google_project.project.project_id
-#   network    = google_compute_network.vpc.self_link
-#   subnetwork = module.network_us_central1.subnetwork.self_link
-
-#   remove_default_node_pool = true
-#   initial_node_count       = 1
-
-#   master_auth {
-#     username = ""
-#     password = ""
-#     client_certificate_config {
-#       issue_client_certificate = false
-#     }
-#   }
-#   workload_identity_config {
-#     identity_namespace = "${google_project.project.project_id}.svc.id.goog"
-#   }
-
-#   # Private cluster configuration
-#   private_cluster_config {
-#     enable_private_endpoint = false
-#     enable_private_nodes    = true
-#     master_ipv4_cidr_block  = "172.20.0.0/28"
-#   }
-
-#   # VPC-native cluster configuration
-#   ip_allocation_policy {
-#     cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
-#     services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
-#   }
+#   # TODO: add psp_enabled = false
+#   project                       = google_project.project.project_id
+#   network                       = google_compute_network.vpc.self_link
+#   subnetwork                    = module.network_us_central1.subnetwork.self_link
+#   cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
+#   services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
+#   org_domain                    = var.org_domain
 # }
 
 # module "gke_node_pool" {
 #   source = "../modules/gke_node_pool"
 
-#   # cluster        = module.gke_cluster.name
-#   cluster        = google_container_cluster.primary.name
+#   cluster        = module.gke_cluster.name
 #   location       = "us-central1-a"
 #   machine_type   = "e2-standard-2"
 #   min_node_count = 1
--- modules/gke_cluster/variables.tf
+++ modules/gke_cluster/variables.tf
@@ -4,14 +4,14 @@ variable "network" {}
 variable "subnetwork" {}
 variable "project" {}
 variable "org_domain" {}
-# variable "cluster_secondary_range_name" {}
-# variable "services_secondary_range_name" {}
+variable "cluster_secondary_range_name" {}
+variable "services_secondary_range_name" {}
 
 # you cannot use master, node, Pod, or Service IP range that overlaps with 172.17.0.0/16
-# variable "master_ipv4_cidr_block" {
-#   description = "The IP range in CIDR notation to use for the hosted master network"
-#   default     = "172.16.0.0/28"
-# }
+variable "master_ipv4_cidr_block" {
+  description = "The IP range in CIDR notation to use for the hosted master network"
+  default     = "172.16.0.0/28"
+}
 
 variable "maintenance_start_time" {
   description = "Time window specified for daily maintenance operations. Specify start_time in RFC3339 format HH:MM"
