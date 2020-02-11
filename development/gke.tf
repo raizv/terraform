d5e49252552689182e3f5634f1fd613aa00e61ed
Anton Raizvikh <raizvih@gmail.com>
2020-02-11 14:24:30 -0800

    Create private gke cluster

--- development/gke.tf
+++ development/gke.tf
@@ -24,62 +24,61 @@ module "network_us_central1" {
 
 #   # TODO: add psp_enabled = false
 
-#   project    = google_project.project.project_id
-#   network    = google_compute_network.vpc.self_link
-#   subnetwork = module.network_us_central1.subnetwork.self_link
-#   # cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
-#   # services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
-#   org_domain = var.org_domain
+#   project                       = google_project.project.project_id
+#   network                       = google_compute_network.vpc.self_link
+#   subnetwork                    = module.network_us_central1.subnetwork.self_link
+#   cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
+#   services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
+#   org_domain                    = var.org_domain
 # }
 
-# resource "google_container_cluster" "primary" {
-#   provider   = google-beta
-#   name       = "primary"
-#   location   = "us-central1-a"
-#   project    = google_project.project.project_id
-#   network    = google_compute_network.vpc.self_link
-#   subnetwork = module.network_us_central1.subnetwork.self_link
+resource "google_container_cluster" "primary" {
+  provider   = google-beta
+  name       = "primary"
+  location   = "us-central1-a"
+  project    = google_project.project.project_id
+  network    = google_compute_network.vpc.self_link
+  subnetwork = module.network_us_central1.subnetwork.self_link
 
-#   remove_default_node_pool = true
-#   initial_node_count       = 1
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-#   master_auth {
-#     username = ""
-#     password = ""
+  master_auth {
+    username = ""
+    password = ""
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+  workload_identity_config {
+    identity_namespace = "${google_project.project.project_id}.svc.id.goog"
+  }
 
-#     client_certificate_config {
-#       issue_client_certificate = false
-#     }
-#   }
-#   workload_identity_config {
-#     identity_namespace = "${google_project.project.project_id}.svc.id.goog"
-#   }
+  # Private cluster configuration
+  private_cluster_config {
+    enable_private_endpoint = false
+    enable_private_nodes    = true
+    master_ipv4_cidr_block  = "172.20.0.0/28"
+  }
 
-#   # Private cluster configuration
-#   private_cluster_config {
-#     enable_private_endpoint = false
-#     enable_private_nodes    = true
-#     master_ipv4_cidr_block  = "172.20.0.0/28"
-#   }
+  # VPC-native cluster configuration
+  ip_allocation_policy {
+    cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
+    services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
+  }
+}
 
-#   # VPC-native cluster configuration
-#   ip_allocation_policy {
-#     cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
-#     services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
-#   }
-# }
+module "gke_node_pool" {
+  source = "../modules/gke_node_pool"
 
-# module "gke_node_pool" {
-#   source = "../modules/gke_node_pool"
+  # cluster        = module.gke_cluster.name
+  cluster        = google_container_cluster.primary.name
+  location       = "us-central1-a"
+  machine_type   = "e2-standard-2"
+  min_node_count = 1
+  max_node_count = 3
+  preemptible    = true
 
-#   # cluster        = module.gke_cluster.name
-#   cluster        = google_container_cluster.primary.name
-#   location       = "us-central1-a"
-#   machine_type   = "e2-standard-2"
-#   min_node_count = 1
-#   max_node_count = 3
-#   preemptible    = true
-
-#   project         = google_project.project.project_id
-#   service_account = module.gke_service_account.email
-# }
+  project         = google_project.project.project_id
+  service_account = module.gke_service_account.email
+}
