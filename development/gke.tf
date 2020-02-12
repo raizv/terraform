70e5e21b3d004848def2b7beb0e4de0a49f9dab3
Anton Raizvikh <raizvih@gmail.com>
2020-02-12 11:15:38 -0800

    Create new gke cluster

--- development/gke.tf
+++ development/gke.tf
@@ -11,10 +11,39 @@ module "network_us_central1" {
   network = google_compute_network.vpc.self_link
 }
 
-module "gke_cluster" {
+# module "gke_cluster" {
+#   source = "../modules/gke_cluster"
+
+#   name     = "development"
+#   location = "us-central1-a"
+#   psp      = false
+
+#   org_domain                    = var.org_domain
+#   project                       = google_project.project.project_id
+#   network                       = google_compute_network.vpc.self_link
+#   subnetwork                    = module.network_us_central1.subnetwork.self_link
+#   cluster_secondary_range_name  = module.network_us_central1.subnetwork.secondary_ip_range.0.range_name
+#   services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
+# }
+
+# module "gke_node_pool" {
+#   source = "../modules/gke_node_pool"
+
+#   cluster        = module.gke_cluster.name
+#   location       = "us-central1-a"
+#   machine_type   = "e2-standard-2"
+#   min_node_count = 1
+#   max_node_count = 3
+#   preemptible    = true
+
+#   project         = google_project.project.project_id
+#   service_account = module.gke_service_account.email
+# }
+
+module "gke_cluster_new" {
   source = "../modules/gke_cluster"
 
-  name     = "development"
+  name     = "development-new"
   location = "us-central1-a"
   psp      = false
 
@@ -26,10 +55,10 @@ module "gke_cluster" {
   services_secondary_range_name = module.network_us_central1.subnetwork.secondary_ip_range.1.range_name
 }
 
-module "gke_node_pool" {
+module "gke_node_pool_new" {
   source = "../modules/gke_node_pool"
 
-  cluster        = module.gke_cluster.name
+  cluster        = module.gke_cluster_new.name
   location       = "us-central1-a"
   machine_type   = "e2-standard-2"
   min_node_count = 1
